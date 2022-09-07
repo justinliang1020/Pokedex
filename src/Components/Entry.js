fa650c06196ab8492004b5ab3be7ecc0a8372f64
Justin Liang <justinliang1020@gmail.com>
2022-09-07 14:35:49 -0400

    Planning backend integration

--- src/Components/Entry.js
+++ src/Components/Entry.js
@@ -14,11 +14,12 @@ const Entry = () => {
             height: 300,
           },
     });
-    
+
     const [input, setInput] = useState("pikachu");
     const [name, setName] = useState("pikachu");
     const [id, setId] = useState(25);
     const [spriteUrl, setSpriteUrl] = useState("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png");
+    const [likes, setLikes] = useState(0);
 
     changePokemon = () => {
         fetch(`https://pokeapi.co/api/v2/pokemon/${input.toLowerCase()}`)
@@ -28,6 +29,13 @@ const Entry = () => {
             setId(data.id);
             setSpriteUrl(data.sprites.front_default);
         })
+
+        // fetch current likes
+    }
+
+    likePokemon = () => {
+        // increment likes
+        // post pokemon api like
     }
 
     return (
@@ -35,9 +43,11 @@ const Entry = () => {
             style={styles.container}>
             <Text>Hello, {name}!</Text>
             <Text>ID: {id}</Text>
+            {/* Number of likes */}
             <Image style={styles.tinyLogo} source={{uri: spriteUrl}} />
             <TextInput onChangeText={setInput} value={input}/>
             <Button onPress={this.changePokemon} title="Search" />
+            {/* like button? conditional rendering */}
         </View>
     )
 }
